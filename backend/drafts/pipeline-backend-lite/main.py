d1667a77435dffb35bd0c5edacf56b039dbe8913
NyraO <amina.a.hussain18@gmail.com>
2025-12-05 13:22:18 +0100

    start endpoint 1st

--- backend/drafts/pipeline-backend-lite/main.py
+++ backend/drafts/pipeline-backend-lite/main.py
@@ -1,10 +1,19 @@
 import sys, os, json, traceback, uuid
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
+from pydantic import BaseModel
 from quixstreams import Application
 from logger import get_logger
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 
+# Set a unique consumer group to avoid conflicts
+consumer_group = f"consumer_group_{uuid.uuid4().hex[:8]}"
+
+class PipelineInput(BaseModel):
+    address: str
+    input_topic: str
+    output_topic: str
+    transformations: list[str]
 
 app = FastAPI(
     title="Pipeline Backend Lite",
@@ -12,19 +21,30 @@ app = FastAPI(
     version="2.0.0"
 )
 
+def get_callable_function_for_transformation(transformation_script):
+    
+    local_scope = {}
+    try:
+        exec(transformation_script, {}, local_scope)
+        return local_scope.get("transform", lambda x: x)
+    except Exception as e:
+        print(f"Script Error: {e}")
+        return lambda x: x
 
-# ==========================================
-# SECTION 2: DYNAMIC CODE COMPILER (HELPER)
-# ==========================================
-# This function converts the string from Frontend into a usable Python function
-def compile_user_code(code_str: str, entry_point_name: str):
-    """
-    1. Create a dictionary to serve as the local execution scope.
-    2. Define a global scope (restrict imports if necessary for security).
-    3. Use 'exec(code_str, globals, locals)' to run the string.
-    4. Check if 'entry_point_name' exists in locals.
-    5. Return the actual function object.
-    6. Wrap in try/except to catch SyntaxErrors and return clear messages to frontend.
-    """
-    pass
+@app.post("/start")
+async def process_data(pipeline:PipelineInput):
+    quixApp = Application(
+            broker_address=pipeline.address,
+            auto_offset_reset="earliest",
+            consumer_group=consumer_group,
+        )
+    input_topic = quixApp.topic(name = pipeline.input_topic)
+    output_topic = quixApp.topic(name = pipeline.output_topic)
+    current_dataframe = app.dataframe(input_topic)
 
+    for t_function in pipeline.transformations:
+        transform_func = get_callable_function_for_transformation(t_function)
+        current_dataframe = current_dataframe.apply(transform_func).filter(lambda x: x is not None)
+
+    current_dataframe.to_topic(output_topic)
+    return "done"
