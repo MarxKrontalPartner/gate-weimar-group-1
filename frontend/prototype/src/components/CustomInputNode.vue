1e55c812cea3646ffe3e90f34035ed50bb871fda
ananthu <ask1234560@gmail.com>
2025-12-10 21:02:23 +0100

    graph import name missing fix

--- frontend/prototype/src/components/CustomInputNode.vue
+++ frontend/prototype/src/components/CustomInputNode.vue
@@ -20,7 +20,7 @@ const onInput = (event: InputEvent) => {
   <div class="container dark">
     <input
       id="inputnode"
-      :placeholder="data.content"
+      :value="data.content"
       class="nodrag uk-input input-nodes"
       type="text"
       aria-label="Input"
