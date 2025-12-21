d584547b81ed37b6cf6cbc50ca6e7b2062d638a7
Abiodun Alagbada <abiodunalagbada@gmail.com>
2025-12-21 22:11:07 +0100

    Removed redundancy

--- frontend/prototype/src/components/CustomTransformNode.vue
+++ frontend/prototype/src/components/CustomTransformNode.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import { Position, Handle } from '@vue-flow/core'
 import { CodeEditor } from 'monaco-editor-vue3'
-import { reactive, ref, computed, onMounted, watch } from 'vue'
+import { reactive, ref, computed, watch } from 'vue'
 import { useVueFlow } from '@vue-flow/core'
 import { useRoute } from 'vue-router'
 
-const { updateNodeData, findNode } = useVueFlow()
+const { updateNodeData } = useVueFlow()
 const showModal = ref(false)
 const props = defineProps(['id', 'data', 'isDark'])
 
@@ -14,56 +14,25 @@ const props = defineProps(['id', 'data', 'isDark'])
 const reactiveData = reactive(props.data)
 
 const route = useRoute()
-
 const isInTestArea = computed(() => route.name === 'test-area')
-
-// Local editable copy of the code (prevents reactivity conflicts)
-const localCode = ref<string>('')
-
-onMounted(() => {
-  localCode.value = reactiveData.code ?? ''
-})
 
 watch(
   () => props.data.code,
   (newVal) => {
     if (!showModal.value) {
-      localCode.value = newVal ?? ''
       reactiveData.code = newVal ?? ''
     }
   },
 )
 
-// Sync localCode back to reactiveData when localCode changes
-const handleEditorChange = (newValue: string) => {
-  localCode.value = newValue
-  reactiveData.code = newValue
-}
-
-const onCloseModal = () => {
-  showModal.value = false
-}
-
 const onModalToggle = () => {
   if (isInTestArea.value) {
     return
   }
-
-  if (!showModal.value) {
-    const node = findNode(props.id)
-    if (node?.data && typeof node.data === 'object') {
-      const nodeData = node.data as { code?: string }
-      localCode.value = nodeData.code ?? ''
-      reactiveData.code = nodeData.code ?? ''
-    }
-  }
-
   showModal.value = !showModal.value
 }
 
 const onSave = () => {
-  reactiveData.code = localCode.value
-
   updateNodeData(props.id, {
     code: reactiveData.code,
   })
@@ -90,7 +59,7 @@ const editorOptions = {
     <button
       @click="onModalToggle"
       class="uk-button uk-button-primary uk-button-small"
-      :data-uk-toggle="!isInTestArea ? 'target: #modal-example' : undefined"
+      :uk-toggle="!isInTestArea ? 'target: #modal-example' : undefined"
       style="border-radius: 3px; width: auto"
     >
       Edit Code
@@ -108,23 +77,13 @@ const editorOptions = {
           <h2 class="uk-modal-title">{{ reactiveData.content }}</h2>
           <div class="code-editor-container">
             <CodeEditor
-              :value="localCode"
-              @change="handleEditorChange"
+              v-model:value="reactiveData.code"
               language="python"
               :theme="isDark ? 'vs-dark' : 'vs-light'"
               :options="editorOptions"
             />
           </div>
           <p class="uk-text-right">
-            <button
-              @click="onCloseModal"
-              class="uk-button uk-button-default uk-button-small"
-              type="button"
-              style="margin-right: 10px"
-            >
-              Cancel
-            </button>
-
             <button
               @click="onSave"
               class="uk-button uk-modal-close uk-save-button uk-button-small"
