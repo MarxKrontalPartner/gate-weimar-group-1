ca872840e2843f7b1ce354c3b372c435c47ddb89
Abiodun Alagbada <abiodunalagbada@gmail.com>
2025-12-23 13:51:05 +0100

    I updated and moved some content to TransformEditorModal for clarity

--- frontend/prototype/src/components/CustomTransformNode.vue
+++ frontend/prototype/src/components/CustomTransformNode.vue
@@ -1,79 +1,35 @@
 <script setup lang="ts">
-import { Position, Handle } from '@vue-flow/core'
-import { CodeEditor } from 'monaco-editor-vue3'
+import { Position, Handle, useVueFlow } from '@vue-flow/core'
 import { reactive, ref } from 'vue'
-import { useVueFlow } from '@vue-flow/core'
+import TransformEditorModal from '@/components/TransformEditorModal.vue'
 
 const { updateNodeData } = useVueFlow()
-const showModal = ref(false)
 const props = defineProps(['id', 'data', 'isDark'])
-
-//props specifically for light/dark mode
-
+const showModal = ref(false)
 const reactiveData = reactive(props.data)
 
-const onModalToggle = () => {
-  showModal.value = !showModal.value
-}
-
-const onSave = () => {
-  updateNodeData(props.id, {
-    code: reactiveData.code,
-  })
-  onModalToggle()
-}
-
-const editorOptions = {
-  fontSize: 14,
-  minimap: { enabled: false },
-  automaticLayout: true,
+const onSave = (newCode: string) => {
+  reactiveData.code = newCode
+  updateNodeData(props.id, { code: newCode })
 }
 </script>
 
 <template>
-  <div class="container dark">
+  <div class="container" :class="{ dark: isDark }">
     <Handle type="target" :position="Position.Left" :connectable="1" />
-    <input
-      type="text"
-      v-model="reactiveData.content"
-      class="nodrag uk-input input-nodes"
-      name="transformation-name"
-    />
-
-    <!-- This is a button toggling the modal -->
-    <button
-      @click="onModalToggle"
-      class="uk-button uk-button-primary uk-button-small"
-      uk-toggle="target: #modal-example"
-      style="border-radius: 3px; width: auto"
-    >
+    <input v-model="reactiveData.content" class="nodrag uk-input input-nodes" />
+    <button class="uk-button uk-button-primary uk-button-small edit-code-btn" @click="showModal = true">
       Edit Code
     </button>
-
-    <!-- This is the modal -->
-    <div id="modal-example" uk-modal="esc-close: false; bg-close: false" v-if="showModal">
-      <div class="uk-modal-dialog uk-modal-body">
-        <h2 class="uk-modal-title">{{ reactiveData.content }}</h2>
-        <div class="code-editor-container">
-          <CodeEditor
-            v-model:value="reactiveData.code"
-            language="python"
-            :theme="isDark ? 'vs-dark' : 'vs-light'"
-            :options="editorOptions"
-          />
-        </div>
-        <p class="uk-text-right">
-          <button
-            @click="onSave"
-            class="uk-button uk-modal-close uk-save-button uk-button-small"
-            type="button"
-          >
-            Save
-          </button>
-        </p>
-      </div>
-    </div>
+    <Handle type="source" :position="Position.Right" :connectable="1" />
   </div>
 
-  <Handle type="source" :position="Position.Right" :connectable="1" />
+  <TransformEditorModal
+    :show="showModal"
+    :node-name="reactiveData.content || 'Transform'"
+    :initial-code="reactiveData.code || ''"
+    :is-dark="isDark"
+    @save="onSave"
+    @close="showModal = false"
+  />
 </template>
