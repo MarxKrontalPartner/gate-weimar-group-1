e7298841f654b7e42f756021274d13001a9e620b
Abiodun Alagbada <abiodunalagbada@gmail.com>
2025-12-23 13:43:04 +0100

    I updated this to file

--- frontend/prototype/env.d.ts
+++ frontend/prototype/env.d.ts
@@ -1,1 +1,19 @@
 /// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, any>
+  export default component
+}
+
+declare module 'monaco-editor-vue3' {
+  import type { DefineComponent } from 'vue'
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  export const MonacoEditor: DefineComponent<any, any, any>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  export const CodeEditor: DefineComponent<any, any, any>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const defaultExport: DefineComponent<any, any, any>
+  export default defaultExport
+}
